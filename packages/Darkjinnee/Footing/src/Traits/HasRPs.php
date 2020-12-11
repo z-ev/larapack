0db6e4952e44004cb0680e82bed75e99a0b86fb5
darkjinnee <darkjinnee@gmail.com>
2020-12-11 21:02:44 +0400

    feat(traits): add method masks

--- packages/Darkjinnee/Footing/src/Traits/HasRPs.php
+++ packages/Darkjinnee/Footing/src/Traits/HasRPs.php
@@ -3,13 +3,32 @@
 
 namespace Darkjinnee\Footing\Traits;
 
+use Illuminate\Support\Collection;
 
+/**
+ * Trait HasRPs
+ * @package Darkjinnee\Footing\Traits
+ */
 trait HasRPs
 {
     use HasRoles, HasPermissions;
 
-    public function masks()
+    /**
+     * @return Collection
+     */
+    public function masks(): Collection
     {
-        return $this->load('roles.permissions', 'permissions');
+        $this->load('roles.permissions', 'permissions');
+        $masks = collect();
+
+        $this->roles->each(function ($item) use ($masks) {
+            $masks->push($item->permissions->pluck('mask'));
+        });
+        $masks->push($this->permissions->pluck('mask'));
+
+        $masks = $masks->flatten();
+        $duplicatesKeys = $masks->duplicates()->keys();
+
+        return $masks->except($duplicatesKeys)->values();
     }
 }
